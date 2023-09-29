d4b4393ac3262ee9e7d65506c76b77789489b8f2
Geraldo Luiz <geraldobl58@gmail.com>
2023-09-29 14:13:19 -0300

    feat: Add configuration page

---
+++ src/app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useOrigin } from "@/hooks/use-origin"
+
+import { ApiAlert } from "@/components/ui/api-alert"
+import { Heading } from "@/components/ui/heading"
+
+const SettingsPage = () => {
+  const origin = useOrigin()
+
+  return (
+    <>
+      <Heading
+        title="Configurações"
+        description="Gerenciar os endpoints do sistema."
+      />
+      <ApiAlert 
+        title="NEXT_PUBLIC_URL" 
+        description={`${origin}/api`} 
+        variant="public" 
+      />
+    </>
+  )
+}
+
+export default SettingsPage
